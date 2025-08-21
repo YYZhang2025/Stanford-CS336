0b5754cdcf8aa6cba1cb7051fd463f709b414eeb
YYZhang <yuyangzhang2025@gmail.com>
2025-08-21 11:36:35 +0800

    add utils

--- assignment5-alignment/cs336_alignment/utils.py
+++ assignment5-alignment/cs336_alignment/utils.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 import regex as re
 
@@ -12,10 +13,27 @@ def extract_reference_answer(answer: str, pattern: re.Pattern = ANS_RE) -> str:
     return "[invalid]"
 
 
-def load_json_to_list(file_path: str) -> list[str]:
+def load_json_to_list(file_path: str) -> list[dict]:
     data_list = []
     with open(file_path, "r", encoding="utf-8") as f:
         for line in f:
             data_list.append(json.loads(line.strip()))
 
     return data_list
+
+
+def safe_slug(s: str) -> str:
+    # Replace path separators and any weird chars with '-'
+    return re.sub(r"[^A-Za-z0-9._-]+", "-", s.replace("/", "-").replace("\\", "-"))
+
+
+def get_run_name(prefix: str, config):
+    return f"{prefix}-{safe_slug(config.model_name)}-{config.num_example}-{config.data_path.split('/')[2]}"
+
+
+def save_model_and_tokenizer(model, tokenizer, experiment_name):
+    out_dir = Path(f"./experiments/{experiment_name}")
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    model.save_pretrained(out_dir)
+    tokenizer.save_pretrained(out_dir)
