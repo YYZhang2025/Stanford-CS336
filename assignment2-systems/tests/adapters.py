9f5f3130f3a3d6c6fba231befafa7bbfcc0cc88c
YYZhang <yuyangzhang2025@gmail.com>
2026-01-14 05:41:16 +0800

    update Triton Flash Attention

--- assignment2-systems/tests/adapters.py
+++ assignment2-systems/tests/adapters.py
@@ -34,7 +34,10 @@ def get_flashattention_autograd_function_triton() -> Type:
         A class object (not an instance of the class)
     """
     # For example: return MyTritonFlashAttentionAutogradFunctionClass
-    raise NotImplementedError
+    # raise NotImplementedError
+    from cs336_systems.flash_attention.triton_version import FlashAttention
+
+    return FlashAttention
 
 
 def get_ddp_individual_parameters(module: torch.nn.Module) -> torch.nn.Module:
